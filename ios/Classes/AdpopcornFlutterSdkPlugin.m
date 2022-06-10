a5269da3345cd08e437243b43b309df3cf2d465f
Neo Kim <neo.kim@crepay.com>
2022-06-10 17:08:36 +0900

    Create iOS template with Obj-C

---
+++ ios/Classes/AdpopcornFlutterSdkPlugin.m
@@ -0,0 +1,20 @@
+#import "AdpopcornFlutterSdkPlugin.h"
+
+@implementation AdpopcornFlutterSdkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"adpopcorn_flutter_sdk"
+            binaryMessenger:[registrar messenger]];
+  AdpopcornFlutterSdkPlugin* instance = [[AdpopcornFlutterSdkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
